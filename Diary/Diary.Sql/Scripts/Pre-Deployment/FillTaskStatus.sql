3106641536f4d4dbadfa0d34f0dadd24715b60b6
PavelMPD <PavelMPD82@gmail.com>
2013-09-13 08:52:15 +0300

    Add new project Diary

---
+++ Diary/Diary.Sql/Scripts/Pre-Deployment/FillTaskStatus.sql
@@ -0,0 +1,14 @@
+﻿USE [DIARY]
+
+INSERT INTO [dbo].[TaskStatus]
+([Id], [Name])
+VALUES (1, 'Новая')
+GO
+INSERT INTO [dbo].[TaskStatus]
+([Id], [Name])
+VALUES (2, 'В работе')
+GO
+INSERT INTO [dbo].[TaskStatus]
+([Id], [Name])
+VALUES (3, 'Выполненная')
+GO
---
+++ Diary/Diary/App_Start/PreStartApp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Diary.App_Start
+{
+    /*
+    Framework 4.5
+    [assembly: WebActivator.PreApplicationStartMethod(typeof(PreStartApp), "Start")]
+    public static class PreStartApp
+    {
+        private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+
+        /// <summary>
+        /// Метод запускается один раз перед стартом приложения        
+        /// </summary>
+        public static void Start()
+        {
+            logger.Info("Application PreStart");
+        }
+    }
+    */
+}
