dcc451d29cd39cf427f8b59377775de04d91f7b9
PavelMPD <PavelMPD82@gmail.com>
2013-09-13 10:48:24 +0300

    Add Entity Framework model. Add task statuses on the Tasks page.

---
+++ Diary/Diary/Controllers/TasksController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Data;
+using System.Data.Entity;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Diary.Models;
+
+namespace Diary.Controllers
+{
+    public class TasksController : Controller
+    {
+        DIARYEntities entities = new DIARYEntities();
+        //
+        // GET: /Tasks/
+
+        public ActionResult Index()
+        {
+            List<TaskStatus> list = entities.TaskStatus.ToList();
+
+
+            ViewBag.TaskStatuses = GetTaskStatuses(list);
+            return View(list);
+        }
+
+        public ActionResult AddTask()
+        {
+            return View();
+        }
+
+        private IEnumerable<SelectListItem> GetTaskStatuses(IList<TaskStatus> list)
+        {
+            return list.Select(ts => new SelectListItem() { Text = ts.Name, Value = ts.Id.ToString()});
+        }
+    }
+}
