ec10c1621855a405d85395b2a4886fe1cdd8d125
PavelMPD <PavelMPD82@gmail.com>
2013-12-22 17:00:53 +0400

    MailSender project is added

---
+++ SimpleMail/MailSender/CimUpdateDataResultWrappers/ResultStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailSender.CimUpdateDataResultWrappers
+{
+    public enum ResultStatus
+    {
+        None = 0,
+        Success = 1,
+        Fail = 2
+    }
+}
---
+++ SimpleMail/MailSender/MailBodyBuider.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Common;
+using MailSender.CimUpdateDataResultWrappers;
+
+namespace MailSender
+{
+    public interface IMailBodyBuider
+    {
+        String Build(CimUpdateDataResult updateDataResult);
+    }
+
+    public class MailBodyBuider : IMailBodyBuider
+    {
+        public String Build(CimUpdateDataResult updateDataResult)
+        {
+            String updateDataResultXml = updateDataResult.Serialize();
+
+        }
+    }
+}
