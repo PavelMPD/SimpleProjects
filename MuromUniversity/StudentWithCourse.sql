87063ff218cc6c30fa5e6bac800899045a464ff1
PavelMPD <PavelMPD82@gmail.com>
2014-05-23 14:07:29 +0300

    Add MuromUniversity db.

---
+++ MuromUniversity/StudentWithCourse.sql
@@ -0,0 +1,9 @@
+﻿select
+	s.FirstMidName,
+	s.LastName,
+	s.EnrollmentDate,
+	c.Title,
+	e.Grade
+from dbo.Student s
+	inner join dbo.Enrollment e on e.StudentID = s.StudentID
+	inner join dbo.Course c on c.CourseID = e.CourseID
